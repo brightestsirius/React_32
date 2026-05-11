3c05c93f8bb9127b44ef05abeb3bbc6daa9b3185
brightestsirius <katherina.shipovskaya@boosta.com>
2026-05-11 21:22:40 +0300

    lesson_01

--- lesson_01/src/App.jsx
+++ lesson_01/src/App.jsx
@@ -1,40 +1,14 @@
-import React from "react";
-import Profile from "./components/Profile/Profile";
-
-export default function App() {
-  const count = 0;
-  const isTrue = false;
-  const first = "first";
-  const second = "second";
-  const animals = [`cat`, `dog`, `lion`];
+const App = () => {
+  const userName = `Alina`;
 
   return (
     <>
-      <div>{count}</div>
-      <div>{isTrue ? first : second}</div>
-      ---
-      <div>1. {isTrue && first}</div>
-      <div>2. {count && first}</div>
-      <div>3. {count ?? first}</div>
-      <div>4. {count ? first : undefined}</div>
-      <div>5. {count ? first : null}</div>
-      <div>6. {Boolean(count) && first}</div>
-      ---
-      <ul>
-        {animals.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-      ---
-      <Profile
-        name={"Katya"}
-        role={"Lector"}
-        avatarUrl={
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX4q0-hFsRa8s1kzziYZVHIW1zg-yH0S2POA&s"
-        }
-        skills={["React", "Java", "Designe"]}
-        isOnline={true}
-      />
+      <h1>
+        Hello, <b>{userName}</b>!
+      </h1>
+      <h2>Description: {10+20}</h2>
     </>
   );
-}
+};
+
+export default App;
